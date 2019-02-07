338099485fc27d55bda7810004c783eaf6b20ce0
Jeff Calaway <jeff@heycalaway.com>
2019-02-07 09:41:52 -0600

    Course Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    styleUrls: ['app.component.scss'],
+    template:`
+        <div class="app">
+            <button (click)="handleClick()">Change name</button>
+
+            <input
+                type="text"
+                [ngModel]="name"
+                (ngModelChange)="handleChange($event)"
+                >
+
+            <input
+                type="text"
+                [(ngModel)]="name"
+                >
+
+            <div>{{ name }}</div>
+        </div>
+    `
 })
 export class AppComponent {
-  title = 'app';
+    name: string = 'Jeff';
+
+    handleClick() {
+        this.name = 'Calaway';
+    }
+
+    handleChange(value: string) {
+        this.name = value;
+    }
 }
